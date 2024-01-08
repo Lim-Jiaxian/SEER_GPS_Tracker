28d76dd65e31163a956f43138c5c29e9a58fb267
Lim Jia Xian <77652380+Lim-Jiaxian@users.noreply.github.com>
2024-01-08 14:52:10 +0800

    Temp progress on security update for php

--- php/GpsPostData.php
+++ php/GpsPostData.php
@@ -1,6 +1,14 @@
 <?php
 // Obtain connection to MySQL database
 require_once('dbConnection.php');
+$json = file_get_contents('php://input');
+$gpsdata = json_decode($json, true);
+
+$sqlstatement = $mysqliconn->prepare("INSERT INTO Records (SecGuardID, RecordTime, Altitude, Latitude, Longitude) VALUES (?, ?, ?, ?, ?);");
+$sqlstatement->bind_param("isddd", $gpsdata['secguardid'], $gpsdata['recordtime'], $gpsdata['altitude'], $gpsdata['latitude'], $gpsdata['longitude']);
+$sqlstatement->execute();
+
+
 if ($_SERVER["REQUEST_METHOD"] !== 'POST')
 {
     die("ERROR: Request method not POST");
