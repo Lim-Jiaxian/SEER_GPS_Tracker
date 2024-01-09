b03a4f172f91e8943e9245a9f46ffee8cb99c778
Lim Jia Xian <limjiaxian2@gmail.com>
2024-01-09 18:42:32 +0800

    Updated PHP files & added comments

--- php/GeoDBConnection.php
+++ php/GeoDBConnection.php
@@ -1,22 +1,19 @@
 <?php
-$serverhostname = "localhost";
+//Database connection variables
+$serverhostname = "localhost"; 
 $username = "root";
 $password = "";
 $database = "geotrackdb";
+
 //Establish connection to MySQL database
 $mysqliconn = mysqli_connect($serverhostname, $username, $password, $database);
 //Check connection
 if ($mysqliconn->connect_errno) {
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
+    //Display error message and exit script
+    die("Failed to connect to MySQL: " . mysqli_connect_error()); 
     exit();
 }
+
+//Show success message if connection is successful
 echo "Connected successfully";
-
-$json = file_get_contents('php://input');
-$gpsdata = json_decode($json, true);
-
-$sqlstatement = $mysqliconn->prepare("INSERT INTO Records (SecGuardID, RecordTime, Altitude, Latitude, Longitude) VALUES (?, ?, ?, ?, ?);");
-$sqlstatement->bind_param("isddd", $gpsdata['secguardid'], $gpsdata['recordtime'], $gpsdata['altitude'], $gpsdata['latitude'], $gpsdata['longitude']);
-$sqlstatement->execute();
-
 ?>
