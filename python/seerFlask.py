44829725eefb231deebabee786f6f8b42a364384
Cayden <S10242778@connect.np.edu.sg>
2024-01-21 22:33:52 +0800

    Flask Implementation Works

--- python/seerFlask.py
+++ python/seerFlask.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify , send_file
-from flask_cors import CORS
+#from flask_cors import CORS
 import subprocess
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 @app.route('/run-script', methods=['POST'])
 def home():
-    subprocess.run(["python", "seer.py"])
+    subprocess.run(["python", "python\seer.py"])
     return jsonify({'message': 'success'})
     #return send_file('images/plot.png', mimetype='image/png')
 
