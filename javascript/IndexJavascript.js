8d52b4d0d3ea00308d0dd31a560b34a39025fa1a
Fellow-User <S10242778@connect.np.edu.sg>
2024-02-03 16:19:41 +0800

    Responsiveness Update

--- javascript/IndexJavascript.js
+++ javascript/IndexJavascript.js
@@ -11,3 +11,34 @@ function runSeer() {
     });
 }
 
+window.onload = function() {
+    document.querySelector('.dropdown-btn').addEventListener('click', function() {
+        var navLinks = document.querySelector('.nav-links');
+        if (navLinks.style.display === "none") {
+            navLinks.style.display = "flex";
+        } else {
+            navLinks.style.display = "none";
+        }
+    });
+};
+
+function toggleMenu() {
+    document.querySelector('.dropdown-btn').addEventListener('click', function() {
+        var navLinks = document.querySelector('.nav-links');
+        if (navLinks.style.display === "none") {
+            navLinks.style.display = "flex";
+        } else {
+            navLinks.style.display = "none";
+        }
+    });
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("nav-links");
+    if (x.className === "nav-links") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-links";
+    }
+  }
